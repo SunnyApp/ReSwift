ed757f12e849f496b258a4e48b88b10a5d096f08
ericmartineau <smartytime@gmail.com>
2019-03-29 09:28:55 -0700

    Fixing casing

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "Reswift",
+    name: "ReSwift",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Reswift",
-            targets: ["Reswift"]),
+            name: "ReSwift",
+            targets: ["ReSwift"]),
     ],
     dependencies: [
         
@@ -18,9 +18,9 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Reswift",
+            name: "ReSwift",
             dependencies: [],
-            path: "Reswift")
+            path: "ReSwift")
         
     ]
 )
