789566e110e79f0b0a00729f72050a5018f89750
ericmartineau <smartytime@gmail.com>
2019-03-29 09:11:49 -0700

    Updating package

--- Package.swift
+++ Package.swift
@@ -1,10 +1,28 @@
 import PackageDescription
 
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
 let package = Package(
-    name: "ReSwift",
-    exclude: [
-      "ReSwiftTests",
-      "Carthage",
-      "Docs"
+    name: "Reswift",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Reswift",
+            targets: ["Reswift"]),
+    ],
+    dependencies: [
+        
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Reswift",
+            dependencies: [],
+            path: "Reswift")
+        
     ]
 )
